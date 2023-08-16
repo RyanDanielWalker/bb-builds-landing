3b8a0387d3821192ed2aed9e19b8ac25a3a8d4fb
Ryan Walker <ryandanielwalker@gmail.com>
2023-08-16 08:43:32 -0700

    ✨ add Link, home page

---
+++ src/containers/NavBar/Link.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react"
+import { SelectedPage } from "@/shared/types"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+
+type LinkProps = {
+  page: string
+  selectedPage: SelectedPage
+  setSelectedPage: (page: SelectedPage) => void
+}
+
+const Link: FC<LinkProps> = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage
+
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-orange" : ""}
+        transition duration-500 hover:text-dark-gray
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
--- src/containers/NavBar/index.tsx
+++ src/containers/NavBar/index.tsx
@@ -1,8 +1,59 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import eye_logo from "@/assets/photo/logo_large.png"
+import useMediaQuery from "@/hooks/useMediaQuery"
+import Link from "./Link"
+import { SelectedPage } from "@/shared/types"
 
-const NavBar: FC = () => {
+type NavBarProps = {
+  isTopOfPage: boolean
+  selectedPage: SelectedPage
+  setSelectedPage: (page: SelectedPage) => void
+}
+
+const NavBar: FC<NavBarProps> = ({
+  isTopOfPage,
+  selectedPage,
+  setSelectedPage,
+}) => {
   const flexBetween = "flex items-center justify-between"
-  return <div className={`${flexBetween} fixed top-0 z-30 w-full`}>index</div>
+
+  return (
+    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${flexBetween} mx-auto w-5/6`}>
+        <div className={`${flexBetween} w-full gap-16`}>
+          <img style={{ height: "30px" }} alt="logo" src={eye_logo} />
+
+          <div className={`${flexBetween} w-full`}>
+            <div className={`${flexBetween} gap-8 text-sm`}>
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="About"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Services"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+            <div className={`${flexBetween} gap-8`}>
+              <p>Contact</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NavBar
