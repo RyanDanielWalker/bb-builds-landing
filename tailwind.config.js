07491d8f16e312667bdf31a43dde83b946336e3f
Ryan Walker <ryandanielwalker@gmail.com>
2023-08-15 20:41:44 -0700

    🔧 tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black": "#20272B",
+        "dark-gray": "#393F47",
+        "blue": "#1C54A4",
+        "orange": "#FA6A4D",
+        "cream": "#E7E7E1"
+    },
+    fontFamily: {
+      rocGrotesk: ["Roc Grotesk", "sans-serif"],
+    }, 
   },
+  screens: {
+    xs: "480px",
+    sm: "768px",
+    md: "1060px",
+  }
+},
   plugins: [],
 }
