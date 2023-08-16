1f81da78998634c20341629e772f39a335229244
Ryan Walker <ryandanielwalker@gmail.com>
2023-08-16 06:52:13 -0700

    ➕🔧 add svgr, adjust relative path configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
   resolve: {
-    alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
-  }
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 })
